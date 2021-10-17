14c1f71c67777cc0b6c063c31528ac01a22e00eb
Ugo BENSAID <ugo.bensaid@epitech.eu>
2021-10-17 13:26:48 +0200

    create Client class

---
+++ csharp/Client.cs
@@ -0,0 +1,31 @@
+﻿namespace BankATM
+{
+    public class Client
+    {
+        private string name;
+        private string pinCode;
+        private BankAccount bankAccount;
+
+        public Client(string name, string pinCode, BankAccount bankAccount)
+        {
+            this.name = name;
+            this.pinCode = pinCode;
+            this.bankAccount = bankAccount;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getPinCode()
+        {
+            return pinCode;
+        }
+
+        public BankAccount getBankAccount()
+        {
+            return bankAccount;
+        }
+    }
+}
