6142e84861047a1ac1451966e349fb676d2d6f8f
Ugo BENSAID <ugo.bensaid@epitech.eu>
2021-10-17 13:24:33 +0200

    create csharp project

---
+++ csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BankATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.Write("Please enter your name : ");
+            string name = Console.ReadLine();
+
+            ATMActions.pinCodeCreation(name);
+        }
+    }
+}
